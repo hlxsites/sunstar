20209d99fadc039b76afe67bbbda5a9f9bfd41bf
RitwikSrivastava <45959816+RitwikSrivastava@users.noreply.github.com>
2023-09-21 16:08:39 +0530

    Update columns-small.js

--- blocks/columns-small/columns-small.js
+++ blocks/columns-small/columns-small.js
@@ -13,11 +13,11 @@ export default function decorate(block) {
         const pics = col.querySelectorAll('picture');
         if (pics.length) {
           const picWrapper = pics[0].closest('div');
-            if (picWrapper && picWrapper.children.length === pics.length) {
-              // pictures (either wrapped in achors, or otherwise)
-              // are only content in the column
-              picWrapper.classList.add('columns-small-img-col');
-            }
+          if (picWrapper && picWrapper.children.length === pics.length) {
+            // pictures (either wrapped in achors, or otherwise)
+            // are only content in the column
+            picWrapper.classList.add('columns-small-img-col');
+          }
         }
       }
     });
