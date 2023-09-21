7ce33cf48d4cec9697f17d6623af00bd826fe21f
Ritwik Srivastava <ritwik@Ritwiks-MacBook-Pro-2.local>
2023-09-21 15:57:32 +0530

    fix

--- blocks/columns-small/columns-small.js
+++ blocks/columns-small/columns-small.js
@@ -11,32 +11,32 @@ export default function decorate(block) {
       wrapImgsInLinks(col);
       if (!textOnlyColBlock) {
         const pics = col.querySelectorAll('picture');
-          if (pics.length) {
-            const picWrapper = pics[0].closest('div');
-              if (picWrapper && picWrapper.children.length === pics.length) {
-                // pictures (either wrapped in achors, or otherwise)
-                // are only content in the column
-                picWrapper.classList.add('columns-small-img-col');
-              }
-          }
+        if (pics.length) {
+          const picWrapper = pics[0].closest('div');
+            if (picWrapper && picWrapper.children.length === pics.length) {
+              // pictures (either wrapped in achors, or otherwise)
+              // are only content in the column
+              picWrapper.classList.add('columns-small-img-col');
+            }
+        }
       }
     });
   });
 
-    // decorate columns-small with description content
+  // decorate columns-small with description content
   [...block.children].forEach((row) => {
     const cells = row.querySelectorAll('div:not(.columns-small-img-col)');
     if (cells.length) {
       [...cells].forEach((content) => {
-      content.classList.add('description');
-      const contentWrapper = document.createElement('div');
-      contentWrapper.classList.add('description-wrapper');
-      const contentParent = content.parentElement;
-      contentParent.insertBefore(contentWrapper, content);
-      contentWrapper.appendChild(content);
-      if (textOnlyColBlock) {
-        content.classList.add('text-only');
-      }
+        content.classList.add('description');
+        const contentWrapper = document.createElement('div');
+        contentWrapper.classList.add('description-wrapper');
+        const contentParent = content.parentElement;
+        contentParent.insertBefore(contentWrapper, content);
+        contentWrapper.appendChild(content);
+        if (textOnlyColBlock) {
+          content.classList.add('text-only');
+        }
       });
     }
   });
