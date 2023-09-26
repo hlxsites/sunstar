ad2063ffbd7c99712aacbf158dde86d1840a7a07
David Bosschaert <bosschae@adobe.com>
2023-09-26 15:44:49 +0200

    Carousel navigation

--- blocks/career-carousel/career-carousel.js
+++ blocks/career-carousel/career-carousel.js
@@ -4,13 +4,18 @@ function filterIncompleteEntries(json) {
   return json.data.filter((e) => e.image !== '' && e['career-quote'] !== '0' && e['career-jobtitle'] !== '0');
 }
 
-function scrollToCard(idx, card, prevCard, slides) {
+function scrollToCard(idx, card, precedingCard, slides, span) {
   const rect = card.getBoundingClientRect();
+
+  // set the style on the active button
+  const buttons = document.querySelectorAll('.career-slides-nav span');
+  buttons.forEach((b) => b.classList.remove('active-nav'));
+  span.classList.add('active-nav');
 
   // Compute the gap to add to the location
   let gap = 0;
-  if (prevCard) {
-    const prevRect = prevCard.getBoundingClientRect();
+  if (precedingCard) {
+    const prevRect = precedingCard.getBoundingClientRect();
     gap = rect.x - (prevRect.x + prevRect.width);
   }
 
@@ -37,60 +42,69 @@ export default async function decorate(block) {
     const img = document.createElement('img');
     img.src = data[i].image;
     img.alt = data[i].pagename;
-    fig.appendChild(img);
-    a.appendChild(fig);
+    fig.append(img);
+    a.append(fig);
 
     const bq = document.createElement('blockquote');
     bq.textContent = data[i]['career-quote'];
-    a.appendChild(bq);
+    a.append(bq);
 
     const nm = document.createElement('h6');
     nm.textContent = data[i].pagename;
-    a.appendChild(nm);
+    a.append(nm);
 
     const role = document.createElement('p');
     role.textContent = data[i]['career-jobtitle'];
-    a.appendChild(role);
+    a.append(role);
 
     const link = document.createElement('button');
     link.textContent = 'Read More '; // TODO
     const arrow = document.createElement('img');
     arrow.src = '/icons/angle-right-blue.svg';
     arrow.classList.add('icon-angle-right-blue');
-    link.appendChild(arrow);
-    a.appendChild(link);
-    div.appendChild(a);
+    link.append(arrow);
+    a.append(link);
+    div.append(a);
 
-    careerSlides.appendChild(div);
+    careerSlides.append(div);
     slideDivs.push(div);
   }
-  careerSlider.appendChild(careerSlides);
+  careerSlider.append(careerSlides);
 
   const navButtons = document.createElement('div');
   navButtons.classList.add('career-slides-nav');
 
-  const al = document.createElement('a');
-  al.textContent = '<';
-  al.onclick = () => careerSlides.scrollTo(0, careerSlides.scrollHeight);
-  navButtons.appendChild(al);
-
+  let firstBtn;
+  let lastBtn;
   for (let i = 0; i < data.length; i += 1) {
-    const a = document.createElement('a');
-    a.textContent = `${i}`;
     const prevDiv = i > 0 ? slideDivs[i - 1] : null;
-    a.onclick = () => scrollToCard(i, slideDivs[i], prevDiv, careerSlides);
-    navButtons.appendChild(a);
+
+    const s = document.createElement('span');
+    if (i === 0) {
+      s.classList.add('active-nav');
+    }
+    s.tabIndex = '-1';
+    s.onclick = () => scrollToCard(i, slideDivs[i], prevDiv, careerSlides, s);
+    navButtons.append(s);
+
+    if (i === 0) {
+      firstBtn = s;
+    } else if (i === data.length - 1) {
+      lastBtn = s;
+    }
   }
+  const la = document.createElement('img');
+  la.src = '/icons/angle-left-blue.svg';
+  la.classList.add('btn-angle');
+  la.onclick = () => firstBtn.onclick();
+  navButtons.prepend(la);
 
-  const ar = document.createElement('a');
-  ar.textContent = '>';
-  ar.onclick = () => {
-    const div = slideDivs[slideDivs.length - 1];
-    const rect = div.getBoundingClientRect();
-    careerSlides.scrollTo(rect.right, careerSlides.scrollHeight);
-  };
-  navButtons.appendChild(ar);
-  careerSlider.appendChild(navButtons);
+  const ra = document.createElement('img');
+  ra.src = '/icons/angle-right-blue.svg';
+  ra.classList.add('btn-angle');
+  ra.onclick = () => lastBtn.onclick();
+  navButtons.append(ra);
+  careerSlider.append(navButtons);
 
-  block.appendChild(careerSlider);
+  block.append(careerSlider);
 }
