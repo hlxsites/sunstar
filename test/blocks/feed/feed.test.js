e7c0ebb433130664f9f275a5ef798defd6e698c2
Satya Deep Maheshwari <satyam@adobe.com>
2023-10-04 13:01:14 +0530

    Rename category to type and topic to category

--- test/blocks/feed/feed.test.js
+++ test/blocks/feed/feed.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-/* global describe before it after */
+/* global describe it */
 
 import { expect, assert } from '@esm-bundle/chai';
 import { readFile } from '@web/test-runner-commands';
@@ -9,41 +9,34 @@ document.write(await readFile({ path: './feed.plain.html' }));
 const feed = {};
 
 describe('Feed Block', async () => {
-  before(async () => {
-    const mod = await import('../../../blocks/feed/feed.js');
-    Object
-      .keys(mod)
-      .forEach((func) => {
-        feed[func] = mod[func];
-      });
-
-    const index = JSON.parse(await readFile({ path: './query-index.json' }));
-
-    sinon.stub(window, 'fetch').callsFake((v) => {
-      const queryIndex = '/query-index.json';
-      if (v.startsWith(queryIndex)) {
-        return {
-          ok: true,
-          json: () => index,
-        };
-      }
-      return {
-        ok: false,
-        json: () => ({
-          limit: 0, offset: 0, total: 0, data: [],
-        }),
-        text: () => '',
-      };
-    });
-  });
-
-  after(() => {
-    sinon.restore();
-  });
-
   it('Feed Block Decoration', async () => {
     const block = document.querySelector('.feed');
     try {
+      const index = JSON.parse(await readFile({ path: './query-index.json' }));
+      await sinon.stub(window, 'fetch').callsFake((v) => {
+        // eslint-disable-next-line max-len
+        const queryIndex = '/query-index.json';
+        if (v.startsWith(queryIndex)) {
+          return {
+            ok: true,
+            json: () => index,
+          };
+        }
+        return {
+          ok: false,
+          json: () => ({
+            limit: 0, offset: 0, total: 0, data: [],
+          }),
+          text: () => '',
+        };
+      });
+      const mod = await import('../../../blocks/feed/feed.js');
+      Object
+        .keys(mod)
+        .forEach((func) => {
+          feed[func] = mod[func];
+        });
+
       const decoratedBlock = await feed.default(block); // The decorate method is the default one
       const parser = new DOMParser();
       const expected = parser.parseFromString(await readFile({ path: './feed.decorated.html' }), 'text/html');
@@ -52,6 +45,7 @@ describe('Feed Block', async () => {
       assert.fail(e);
     } finally {
       window.index = {}; // Reset cache
+      sinon.restore();
     }
   });
 });
