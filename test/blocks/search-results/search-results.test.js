7c79d204b89e380ea98bc0cb03b427dc9ee50b78
Meet Kumar Jain <meetkjain83@gmail.com>
2023-10-29 16:37:24 +0530

    Fixing the lint issues coming from test/blocks/search-results/search-results.test.js

--- test/blocks/search-results/search-results.test.js
+++ test/blocks/search-results/search-results.test.js
@@ -91,27 +91,18 @@ describe('Search Results', () => {
     expect(res1.classList.toString()).to.equal('search-result');
     const res1h3 = res1.children[1];
     expect(res1h3.nodeName).to.equal('P');
-    // const res1h3a = res1h3.children[0];
-    // expect(res1h3a.nodeName).to.equal('A');
-    // expect(res1h3a.href.endsWith('/news/a/')).to.be.true;
 
     const res2 = block.children[3];
     expect(res2.nodeName).to.equal('DIV');
     expect(res2.classList.toString()).to.equal('search-result');
     const res2h3 = res2.children[1];
     expect(res2h3.nodeName).to.equal('P');
-    // const res2h3a = res2h3.children[0];
-    // expect(res2h3a.nodeName).to.equal('A');
-    // expect(res2h3a.href.endsWith('/news/c/')).to.be.true;
 
     const res3 = block.children[4];
     expect(res3.nodeName).to.equal('DIV');
     expect(res3.classList.toString()).to.equal('search-result');
     const res3h3 = res3.children[1];
     expect(res3h3.nodeName).to.equal('P');
-    // const res3h3a = res3h3.children[0];
-    // expect(res3h3a.nodeName).to.equal('A');
-    // expect(res3h3a.href.endsWith('/news/d/')).to.be.true;
 
     const pageWidget = block.children[5];
     expect(pageWidget.className.toString()).to.equal('pagination');
@@ -178,9 +169,6 @@ describe('Search Results', () => {
     expect(res1.classList.toString()).to.equal('search-result');
     const res1h3 = res1.children[1];
     expect(res1h3.nodeName).to.equal('P');
-    // const res1h3a = res1h3.children[0];
-    // expect(res1h3a.nodeName).to.equal('A');
-    // expect(res1h3a.href.endsWith('/jp/news/a/')).to.be.true;
 
     const pageWidget = block.children[3];
     expect(pageWidget.className.toString()).to.equal('pagination');
