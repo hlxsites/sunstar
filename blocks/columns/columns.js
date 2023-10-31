2c808b8401715cf9b7321027c70622508b83ed78
Dragos Cristian Bute <dbute@dragoss-mbp.eur.adobe.com>
2023-10-31 12:52:07 +0200

    fixed image and text in column bug

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -25,7 +25,7 @@ export default function decorate(block) {
         const pics = col.querySelectorAll('picture');
         if (pics.length) {
           const picWrapper = pics[0].closest('div');
-          if (picWrapper && picWrapper.children.length === pics.length) {
+          if (picWrapper) {
             // pictures (either wrapped in achors, or otherwise)
             // are only content in the column
             picWrapper.classList.add('img-col');
