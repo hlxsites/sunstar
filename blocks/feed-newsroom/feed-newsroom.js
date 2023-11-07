91909612baa2140ef95186e942e1b4f0b8713b73
Meet Kumar Jain <meetkjain83@gmail.com>
2023-11-08 01:39:06 +0530

    Updating the filter and year form

--- blocks/feed-newsroom/feed-newsroom.js
+++ blocks/feed-newsroom/feed-newsroom.js
@@ -126,12 +126,22 @@ export default async function decorate(block) {
   });
   // Creation of filter and year buttons
   const filterDiv = document.createElement('div');
-  const div1 = document.createElement('div');
-  const div2 = document.createElement('div');
-  div1.innerText = 'Year';
-  div2.innerText = 'Filter';
-  filterDiv.appendChild(div1);
-  filterDiv.appendChild(div2);
+  // const div1 = document.createElement('div');
+  // const div2 = document.createElement('div');
+  // div1.innerText = 'Year';
+  // div2.innerText = 'Filter';
+  // filterDiv.appendChild(div1);
+  // filterDiv.appendChild(div2);
+  filterDiv.innerHTML = `<form action="#">
+  <div class="filter-nav">
+    <span>
+      <select class="form-control" name="" id="news_year">
+        <option value="">Year</option>
+        <option value="2023">2023</option><option value="2022">2022</option><option value="2021">2021</option><option value="2020">2020</option><option value="2019">2019</option><option value="2018">2018</option><option value="2017">2017</option><option value="2016">2016</option><option value="2015">2015</option><option value="2014">2014</option><option value="2013">2013</option><option value="2012">2012</option><option value="2011">2011</option><option value="2010">2010</option><option value="2009">2009</option><option value="2008">2008</option><option value="2007">2007</option>                  </select>
+    </span>
+    <button data-nonce="8411f43402" data-post_type="news" class="btn btn btn-outline btn-outline-primary btn-filter btn-filter-js" id="news_filter">FILTER</button>
+  </div>
+</form>`;
 
   if (block.parentNode) {
     block.parentNode.replaceChild(builtBlock, block);
