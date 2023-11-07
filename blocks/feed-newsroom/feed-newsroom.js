1d6cf8566b21acfc265f3d1ac7519c430709f845
Meet Kumar Jain <meetkjain83@gmail.com>
2023-11-08 05:10:28 +0530

    Seggregating steps into functions

--- blocks/feed-newsroom/feed-newsroom.js
+++ blocks/feed-newsroom/feed-newsroom.js
@@ -135,7 +135,7 @@ export default async function decorate(block) {
 
   const omitPageTypes = getMetadataNullable('omit-page-types');
   // eslint-disable-next-line prefer-arrow-callback
-  let results = queryObj.where(function filterElements(el) {
+  const results = queryObj.where(function filterElements(el) {
     const elPageType = (el.pagetype ?? '').trim().toLowerCase();
     let match = false;
     match = (!omitPageTypes || !(omitPageTypes.split(',').includes(elPageType)));
@@ -155,7 +155,7 @@ export default async function decorate(block) {
         <option value="">Year</option>
         <option value="2023">2023</option><option value="2022">2022</option><option value="2021">2021</option><option value="2020">2020</option><option value="2019">2019</option><option value="2018">2018</option><option value="2017">2017</option><option value="2016">2016</option><option value="2015">2015</option><option value="2014">2014</option><option value="2013">2013</option><option value="2012">2012</option><option value="2011">2011</option><option value="2010">2010</option><option value="2009">2009</option><option value="2008">2008</option><option value="2007">2007</option>                  </select>
     </span>
-    <button data-nonce="8411f43402" data-post_type="news" class="btn btn btn-outline btn-outline-primary btn-filter btn-filter-js" id="news_filter">FILTER</button>
+    <button data-nonce="8411f43402" data-post_type="news" " id="news_filter">FILTER</button>
   </div>
 </form>`;
   const uniqYears = Array.from(new Set(results.map((x) => { const itsDate = getFormattedDate(new Date(parseInt(x[blockCfg.sort.trim().toLowerCase()], 10))).split(', '); return parseInt(itsDate[itsDate.length - 1], 10); })));
@@ -167,8 +167,8 @@ export default async function decorate(block) {
     console.log(results);
     searchResults = results.filter((x) => { const itsDate = getFormattedDate(new Date(parseInt(x[blockCfg.sort.trim().toLowerCase()], 10))).split(', '); return (parseInt(itsDate[itsDate.length - 1], 10) === searchYear); });
     console.log(searchResults);
-    results = searchResults;
-    // loadResults(block, blockType, results, blockCfg, chunk, filterDiv);
+    block.innerHTML = '';
+    loadResults(block, blockType, searchResults, blockCfg, chunk, filterDiv);
   });
   loadResults(block, blockType, results, blockCfg, chunk, filterDiv);
 }
