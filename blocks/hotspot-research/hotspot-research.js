34c9194bc9404a096529334756b37d16d623ae1b
Meet Kumar Jain <meetkjain83@gmail.com>
2024-01-11 21:00:00 +0530

    Implementing the Code of hotspot

---
+++ blocks/hotspot-research/hotspot-research.js
@@ -0,0 +1,24 @@
+// media query match that indicates mobile/tablet width
+const isDesktop = window.matchMedia('(min-width: 900px)');
+
+export default function decorate(block) {
+  [...block.children].forEach((row, r) => {
+    if (r > 0) {
+      const nexticondiv = document.createElement('div');
+      nexticondiv.style.left = [...row.children][1].textContent;
+      nexticondiv.style.top = [...row.children][2].textContent;
+      console.log([...row.children][0].innerHTML);
+      nexticondiv.setAttribute('data', [...row.children][0].textContent);
+      console.log(nexticondiv);
+      nexticondiv.addEventListener('click', () => {
+        if (!isDesktop.matches) {
+          const isOnClick = nexticondiv.classList.contains('onclick');
+          if (document.querySelector('.hotspot .onclick')) document.querySelector('.hotspot .onclick').classList.remove('onclick');
+          if (!isOnClick) nexticondiv.classList.add('onclick');
+        }
+      });
+      row.after(nexticondiv);
+      row.remove();
+    }
+  });
+}
